b186ae1a9fb7b61837967a90daef196e620471a8
rebirthmonkey <rebirthmonkey@gmail.com>
2023-03-06 14:58:02 +0800

    udpate kubebuilder-at

--- 30_crd-operator/60_kubebuilder/kubebuilder-at/controllers/at_controller.go
+++ 30_crd-operator/60_kubebuilder/kubebuilder-at/controllers/at_controller.go
@@ -22,6 +22,7 @@ import (
 	"strings"
 	"time"
 
+	atv1 "github.com/rebirthmonkey/k8s-dev/kubebuilder-at/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,55 +30,12 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
-
-	atv1 "github.com/rebirthmonkey/k8s-dev/kubebuilder-at/api/v1"
 )
 
-var log = logf.Log.WithName("controller")
-
-// Add creates a new At Controller and adds it to the APIExtManager with default RBAC. The APIExtManager will set fields on the Controller
-// and Start it when the APIExtManager is Started.
-func Add(mgr manager.Manager) error {
-	return add(mgr, newReconciler(mgr))
-}
-
-// newReconciler returns a new reconcile.Reconciler
-func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &AtReconciler{Client: mgr.GetClient(), Scheme: mgr.GetScheme()}
-}
-
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
-func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
-	c, err := controller.New("at-controller", mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to At
-	err = c.Watch(&source.Kind{Type: &atv1.At{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// Watch pods created by At
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &atv1.At{},
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
+var log = logf.Log.WithName("at-controller")
 
 var _ reconcile.Reconciler = &AtReconciler{}
 
@@ -87,107 +45,116 @@ type AtReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=cnat.wukong.com,resources=ats,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=cnat.wukong.com,resources=ats/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=cnat.wukong.com,resources=ats/finalizers,verbs=update
+//+kubebuilder:rbac:groups=at.wukong.com,resources=ats,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=at.wukong.com,resources=ats/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=at.wukong.com,resources=ats/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 func (r *AtReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
 	reqLogger := log.WithValues("namespace", request.Namespace, "at", request.Name)
 	reqLogger.Info("=== Reconciling At")
+
 	// Fetch the At instance
-	instance := &atv1.At{}
-	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	atInstance := &atv1.At{}
+	err := r.Get(context.TODO(), request.NamespacedName, atInstance)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request - return and don't requeue:
+			// Request object not foundPod, could have been deleted after reconcile request - return and don't requeue:
 			return reconcile.Result{}, nil
 		}
 		return reconcile.Result{}, err
 	}
 
 	// If no phase set, default to pending (the initial phase):
-	if instance.Status.Phase == "" {
-		instance.Status.Phase = atv1.PhasePending
+	if atInstance.Status.Phase == "" {
+		atInstance.Status.Phase = atv1.PhasePending
 	}
 
-	// Now let's make the main case distinction: implementing
 	// the state diagram PENDING -> RUNNING -> DONE
-	switch instance.Status.Phase {
+	switch atInstance.Status.Phase {
 	case atv1.PhasePending:
 		reqLogger.Info("Phase: PENDING")
+
 		// As long as we haven't executed the command yet, we need to check if it's time already to act:
-		reqLogger.Info("Checking schedule", "Target", instance.Spec.Schedule)
+		reqLogger.Info("Checking schedule", "Target", atInstance.Spec.Schedule)
 		// Check if it's already time to execute the command with a tolerance of 2 seconds:
-		d, err := timeUntilSchedule(instance.Spec.Schedule)
+		d, err := timeUntilSchedule(atInstance.Spec.Schedule)
 		if err != nil {
 			reqLogger.Error(err, "Schedule parsing failure")
 			// Error reading the schedule. Wait until it is fixed.
 			return reconcile.Result{}, err
 		}
+
 		reqLogger.Info("Schedule parsing done", "Result", fmt.Sprintf("diff=%v", d))
 		if d > 0 {
 			// Not yet time to execute the command, wait until the scheduled time
 			return reconcile.Result{RequeueAfter: d}, nil
 		}
-		reqLogger.Info("It's time!", "Ready to execute", instance.Spec.Command)
-		instance.Status.Phase = atv1.PhaseRunning
+
+		reqLogger.Info("It's time!", "Ready to execute", atInstance.Spec.Command)
+		atInstance.Status.Phase = atv1.PhaseRunning
 	case atv1.PhaseRunning:
 		reqLogger.Info("Phase: RUNNING")
-		pod := newPodForCR(instance)
-		// Set At instance as the owner and controller
-		if err := controllerutil.SetControllerReference(instance, pod, r.Scheme); err != nil {
+
+		executionPod := newExecutionPod(atInstance)
+
+		// Set At atInstance as the owner and controller
+		if err := controllerutil.SetControllerReference(atInstance, executionPod, r.Scheme); err != nil {
 			// requeue with error
 			return reconcile.Result{}, err
 		}
-		found := &corev1.Pod{}
-		err = r.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-		// Try to see if the pod already exists and if not
-		// (which we expect) then create a one-shot pod as per spec:
+
+		foundPod := &corev1.Pod{}
+		err = r.Get(context.TODO(), types.NamespacedName{Name: executionPod.Name, Namespace: executionPod.Namespace}, foundPod)
+		// Try to see if the executionPod already exists and if not
+		// (which we expect) then create a one-shot executionPod as per spec:
 		if err != nil && errors.IsNotFound(err) {
-			err = r.Create(context.TODO(), pod)
+			err = r.Create(context.TODO(), executionPod) // launch the execution pod
 			if err != nil {
 				// requeue with error
 				return reconcile.Result{}, err
 			}
-			reqLogger.Info("Pod launched", "name", pod.Name)
+			reqLogger.Info("Pod launched", "name", executionPod.Name)
+			atInstance.Status.Phase = atv1.PhaseDone
 		} else if err != nil {
 			// requeue with error
 			return reconcile.Result{}, err
-		} else if found.Status.Phase == corev1.PodFailed || found.Status.Phase == corev1.PodSucceeded {
-			reqLogger.Info("Container terminated", "reason", found.Status.Reason, "message", found.Status.Message)
-			instance.Status.Phase = atv1.PhaseDone
+		} else if foundPod.Status.Phase == corev1.PodFailed || foundPod.Status.Phase == corev1.PodSucceeded {
+			reqLogger.Info("Pod terminated", "reason", foundPod.Status.Reason, "message", foundPod.Status.Message)
+			atInstance.Status.Phase = atv1.PhaseDone
 		} else {
-			// don't requeue because it will happen automatically when the pod status changes
+			// don't requeue because it will happen automatically when the executionPod status changes
 			return reconcile.Result{}, nil
 		}
+
 	case atv1.PhaseDone:
 		reqLogger.Info("Phase: DONE")
 		return reconcile.Result{}, nil
+
 	default:
 		reqLogger.Info("NOP")
 		return reconcile.Result{}, nil
 	}
 
 	// Update the At instance, setting the status to the respective phase:
-	err = r.Status().Update(context.TODO(), instance)
+	err = r.Status().Update(context.TODO(), atInstance)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
 
-	// Don't requeue. We should be reconcile because either the pod or the CR changes.
+	// Don't requeue. We should be reconcile because either the executionPod or the CR changes.
 	return reconcile.Result{}, nil
 }
 
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *atv1.At) *corev1.Pod {
+// newExecutionPod returns a busybox pod with the same name/namespace as the At
+func newExecutionPod(cr *atv1.At) *corev1.Pod {
 	labels := map[string]string{
 		"app": cr.Name,
 	}
 	return &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
+			Name:      cr.Name + "-execution-pod",
 			Namespace: cr.Namespace,
 			Labels:    labels,
 		},
